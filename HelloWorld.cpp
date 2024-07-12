b13e7e46c77b2c427a6bd99d4da7f3f834a81d4a
agC86 <105457539+agC86@users.noreply.github.com>
2024-07-12 13:34:42 +0200

    Delete HelloWorld.cpp

--- HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-/*
-	#include <iostream>					// Preprocessor directive, indicates that we use the part of the C++ standard library that gives us input and output (I/O)
-										// This blank line gets ignored by the compiler
-	int main()							// Special function with the main identifier, it is needed to build the program, else it will fail to link.
-	{									// Start of the function body / start main scope
-		std::cout << "Hello world!";	// Statement to print out some text
-		return 0;						// Statement to return a number, main has some special numbers to show whether the program ran succesfully or not
-	}									// End of the function body / end main scope
-*/
